7a559d00f3e635af4f6b9dcaff39fa61955a8b19
taekwan-tony <gtkwan1@naver.com>
2024-09-12 09:37:22 +0900

    Initial commit

---
+++ sas-back/src/main/java/kr/co/sas/SasBackApplication.java
@@ -0,0 +1,13 @@
+package kr.co.sas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SasBackApplication.class, args);
+	}
+
+}
---
+++ sas-back/src/test/java/kr/co/sas/SasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.sas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
