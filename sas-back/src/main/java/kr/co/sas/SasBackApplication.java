5fe2aadd099689e1242ec1afe69022d5a136e1fe
seong3449 <chosujin517@gmail.com>
2024-09-13 17:55:03 +0900

    security exclude

--- sas-back/src/main/java/kr/co/sas/SasBackApplication.java
+++ sas-back/src/main/java/kr/co/sas/SasBackApplication.java
@@ -2,8 +2,9 @@ package kr.co.sas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
 public class SasBackApplication {
 
 	public static void main(String[] args) {
