ed2ad18e382794426ed00efc8e90afe3e28f9409
taekwan-tony <gtkwan1@naver.com>
2024-09-19 08:50:39 +0900

    1:1문의 back 진행중

--- sas-back/src/main/java/kr/co/sas/question/model/service/QuestionService.java
+++ sas-back/src/main/java/kr/co/sas/question/model/service/QuestionService.java
@@ -1,12 +1,20 @@
 package kr.co.sas.question.model.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.co.sas.question.model.dao.QuestionDao;
+import kr.co.sas.question.model.dto.QuestionDTO;
+import kr.co.sas.question.model.dto.QuestionFile;
 
 @Service
 public class QuestionService {
 	@Autowired
 	private QuestionDao questionDao;
+	
+	
 }
