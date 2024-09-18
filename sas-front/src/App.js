5ceef2ac9a4e604964560b2da5d25b7b8106752b
sujin <chosujin517@gmail.com>
2024-09-18 15:08:29 +0900

    회원가입 화면 구현중..

--- sas-front/src/App.js
+++ sas-front/src/App.js
@@ -2,12 +2,15 @@ import "./default.css"; //기본 색, 폰트 변수
 import { Route, Routes } from "react-router-dom";
 import LoginMain from "./components/user/LoginMain";
 import Main from "./components/main/Main";
+import Join from "./components/user/Join";
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <Routes>
         <Route path="/" element={<Main />} />
+        {/* 확인용-수진;user 메인 생기면 옮기겠슴다 */}
+        <Route path="/join" element={<Join />} />
         <Route path="/login/*" element={<LoginMain />} />
       </Routes>
     </div>
---
+++ sas-front/src/components/user/Join.js
@@ -0,0 +1,17 @@
+import "./join.css";
+
+const Join = () => {
+  return (
+    <div className="join-main">
+      <div className="join-wrap">
+        <div className="title">
+          <h1>회원가입</h1>
+        </div>
+      </div>
+      <div className="agree-wrap"></div>
+      <div className="btn-zone"></div>
+    </div>
+  );
+};
+
+export default Join;
