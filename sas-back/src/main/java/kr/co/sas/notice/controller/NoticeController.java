300dd6476fca77b67f0700a9dd35402048d8e90b
taekwan-tony <gtkwan1@naver.com>
2024-09-19 17:43:53 +0900

    공지사항 리스트 backend 구현중

--- sas-back/src/main/java/kr/co/sas/notice/controller/NoticeController.java
+++ sas-back/src/main/java/kr/co/sas/notice/controller/NoticeController.java
@@ -1,7 +1,12 @@
 package kr.co.sas.notice.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,11 @@ import kr.co.sas.notice.model.service.NoticeService;
 public class NoticeController {
 	@Autowired
 	private NoticeService noticeService;
+	
+	@GetMapping(value="/list/{reqPage}/{noticeType}")
+	public ResponseEntity<Map> list(@PathVariable int reqPage, @PathVariable int noticeType){
+		Map map = noticeService.selectNoticeList(reqPage,noticeType);
+		return ResponseEntity.ok(map);
+	}
 
 }
