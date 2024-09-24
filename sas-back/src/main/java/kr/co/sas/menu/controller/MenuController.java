df714b26505c9b5d402d0c9f7eeecf67bbd785c3
mingyu021691 <dlaalsrb02160@gmail.com>
2024-09-24 09:40:18 +0900

    버튼 이름 재설정 (menuview css수정)

--- sas-back/src/main/java/kr/co/sas/menu/controller/MenuController.java
+++ sas-back/src/main/java/kr/co/sas/menu/controller/MenuController.java
@@ -1,7 +1,10 @@
 package kr.co.sas.menu.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,10 @@ import kr.co.sas.menu.model.service.MenuService;
 public class MenuController {
 	@Autowired
 	private MenuService menuService;
-
+	@GetMapping(value="/menuview")
+	public List list() {
+		List list = menuService.selectMenuList();
+		return list;
+	}
 }
+
