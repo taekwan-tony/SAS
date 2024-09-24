a57e3b2d2f25fed9739bfef049bf52b97c6d20c1
makhai92 <makkai9210@gmail.com>
2024-09-24 17:40:59 +0900

    로그인버튼 추가

--- sas-front/src/components/user/UserMain.js
+++ sas-front/src/components/user/UserMain.js
@@ -141,6 +141,11 @@ function UserMain() {
             />
           </form>
         </div>
+        <div className="user-main-login-button">
+          <Link to="/user/login">
+            <button className="user-main-login-btn">로그인</button>
+          </Link>
+        </div>
         <div className="user-box-bell">
           <div className="user-page-box">
             <div className="bellWrapper">
