c32b6c8cdc8bedf8442dd2cb3e2e9a950b0445c8
sujin <chosujin517@gmail.com>
2024-09-25 11:47:55 +0900

    예약 모달 구현중..

--- sas-front/src/components/reservation/ReservationMain.js
+++ sas-front/src/components/reservation/ReservationMain.js
@@ -3,6 +3,8 @@ import Modal from "react-modal";
 import { useRecoilValue } from "recoil";
 import { isUserLoginState } from "../utils/RecoilData";
 import DatePicker from "../utils/DatePicker";
+import { format } from "date-fns";
+import "./reservationModal.css";
 const ReservationMain = () => {
   const [isReserveModalOpen, setIsReserveModalOpen] = useState(false);
   const goTOReserve = () => {
@@ -22,7 +24,7 @@ const ReservationMain = () => {
     },
     content: {
       width: "1000px",
-      height: "500px",
+      height: "max-content",
       zIndex: "150",
       position: "absolute",
       top: "50%",
@@ -32,10 +34,11 @@ const ReservationMain = () => {
       boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
       backgroundColor: "white",
       justifyContent: "center",
-      overflow: "auto",
+      overflow: "hidden",
     },
   };
   const [reservationPage, setReservationPage] = useState(1);
+
   return (
     <>
       <button onClick={goTOReserve} style={{ marginTop: "100px" }}>
@@ -61,10 +64,31 @@ const ReservationMain = () => {
 };
 
 const ReservationModalFirst = () => {
+  const dayNow = new Date();
+  const year = dayNow.getFullYear();
+  const month =
+    dayNow.getMonth() + 1 < 10
+      ? `0${dayNow.getMonth() + 1}`
+      : dayNow.getMonth() + 1;
+  const date =
+    dayNow.getDate() < 10 ? `0${dayNow.getDate()}` : dayNow.getDate();
+  const today = `${year}-${month}-${date}`;
+  const timeNow = format(dayNow, "hh:mm");
+  console.log(timeNow);
+  const [selected, setSelected] = useState(today);
   return (
-    <>
-      <DatePicker />
-    </>
+    <div className="reservation-modal-wrap first">
+      <DatePicker
+        selected={selected}
+        setSelected={setSelected}
+        dayNow={dayNow}
+      />
+      <div className="reservation-info-wrap">
+        <div className="reservation-how-many">
+          <span className="title">인원 수</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- sas-front/src/components/utils/DatePicker.js
+++ sas-front/src/components/utils/DatePicker.js
@@ -4,21 +4,70 @@ import { ko } from "date-fns/locale";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 
-const DatePicker = () => {
-  const [selected, setSelected] = useState();
-
-  let footer = <p>Please pick a day.</p>;
+const DatePicker = (props) => {
+  const selected = props.selected;
+  const setSelected = props.setSelected;
+  const dayNow = props.dayNow;
+  let footer = "";
   if (selected) {
     footer = <p>You picked {format(selected, "PP")}.</p>;
   }
+  const css = `
+
+  button[class^="rdp"] {
+  outline: none;
+}
+  .rdp-root{
+    --rdp-accent-color: var(--main1);
+    --rdp-font-family-accent: LeeSeoyun;
+    --rdp-month_caption-font: bold larger var(--rdp-font-family-accent); /* The font of the month caption. */
+    --rdp-weekday-font: 500 smaller var(--rdp-font-family-accent);
+    font-size: 20px;
+    --rdp-day-height: 3.5rem;
+    --rdp-day-width: 3.5rem;
+  }
+  .rdp-today:not(.rdp-outside) {
+    color: var(--main1);
+}
+    .rdp-chevron {
+      fill: var(--main1);
+    }
+    .rdp-caption {
+        display:none;
+    }
+    .rdp-cell {
+// 일자에 마우스올렸을때 변화감지
+        cursor: default;
+        font-size:18px;
+    }
+    .rdp-head_cell{
+// 요일 폰트 스타일 변경
+        font-size:17px;
+        font-weight:700;
+    }
+    .rdp-day.rdp-day_selected{
+// 선택일자 스타일변경
+        background:#FFC9F4;
+  `;
   return (
-    <DayPicker
-      locale={ko}
-      mode="single"
-      selected={selected}
-      onSelect={setSelected}
-      footer={footer}
-    />
+    <div className="day-picker">
+      <style>{css}</style>
+      <DayPicker
+        locale={ko}
+        mode="single" //한개만 선택할 수 있도록
+        selected={selected}
+        required={true} //필수로 날짜 선택하게
+        onSelect={setSelected}
+        showOutsideDays={true}
+        //오늘 전 날짜는 선택 불가능
+        disabled={{ before: new Date() }}
+        // 처음 시작할 달 설정, 달력 위에 뜨는 연 월 선택여부
+        captionLayout="label"
+        defaultMonth={new Date(dayNow)}
+        startMonth={new Date(dayNow)}
+        endMonth={new Date(dayNow.getFullYear(), dayNow.getMonth() + 3)}
+      />
+    </div>
   );
 };
 
