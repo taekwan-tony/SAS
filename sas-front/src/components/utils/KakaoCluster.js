6d249f70e7119a1769438d1794dac08e267d823f
taekwan-tony <gtkwan1@naver.com>
2024-09-30 21:24:32 +0900

    카카오 맵 클러스터 구현중

--- sas-front/src/components/utils/KakaoCluster.js
+++ sas-front/src/components/utils/KakaoCluster.js
@@ -1,3 +1,36 @@
-const KakaoCluster = () => {};
+import { Map, MapMarker, MarkerClusterer } from "react-kakao-maps-sdk";
+
+const KakaoCluster = () => {
+  return (
+    <Map // 지도를 표시할 Container
+      center={{
+        // 지도의 중심좌표
+        lat: 36.2683,
+        lng: 127.6358,
+      }}
+      style={{
+        // 지도의 크기
+        width: "100%",
+        height: "450px",
+      }}
+      level={14} // 지도의 확대 레벨
+    >
+      <MarkerClusterer
+        averageCenter={true} // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+        minLevel={10} // 클러스터 할 최소 지도 레벨
+      >
+        {positions.map((pos) => (
+          <MapMarker
+            key={`${pos.lat}-${pos.lng}`}
+            position={{
+              lat: pos.lat,
+              lng: pos.lng,
+            }}
+          />
+        ))}
+      </MarkerClusterer>
+    </Map>
+  );
+};
 
 export default KakaoCluster;
